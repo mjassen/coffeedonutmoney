b1ee8e001465229dd6b0fff537c9dd69cdb97e21
Morgan Jassen <mjassen@users.noreply.github.com>
2014-02-22 21:17:54 -0500

    first commit

---
+++ coffeedonutmoney.php
@@ -0,0 +1,51 @@
+<?php
+/* 你好. Hello.
+Coffee Donut Money. 咖啡甜甜圈錢.
+2014-02-22
+MIT license.
+MIT 授權規定.
+
+******
+The MIT License (MIT)
+
+Copyright (c) 2014 Morgan Jassen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+******
+
+
+*/
+?>
+<html>
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+  <meta name="viewport" content="width=device-width; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;">
+  <title>Coffee Donut Money 咖啡甜甜圈錢</title>
+  <style type="text/css">	
+  #wrapper {
+    max-width: 600px;
+  }
+  </style>
+
+</head>
+<body>
+<div id="wrapper">
+
+<?php
+
+?>
+
+<form method="post" action="<?php echo $_SERVER["PHP_SELF"]; ?>" enctype="multipart/form-data">
+
+<textarea name="text_text1" rows="4" cols="30"></textarea>
+<br/>
+<input type="submit" name="submit" value=" 執行 Go → "/>
+
+</form>
+
+</div>
+</body>
